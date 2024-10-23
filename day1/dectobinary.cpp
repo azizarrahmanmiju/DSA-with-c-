0a3e72d2d93a4422c4d2a6d96eb9ab41e33a533e
azizarrahman <azizarrahman01032003@gmail.com>
2024-10-24 04:57:26 +0600

    covert 2 to 10

--- day1/dectobinary.cpp
+++ day1/dectobinary.cpp
@@ -1,21 +1,28 @@
 #include <iostream>
 using namespace std;
 
-int main()
+int dectobinary(int decNum)
 {
-    int decnum = 43;
     int ans = 0;
     int pow = 1;
-
-    while (decnum > 0)
+    while (decNum > 0)
 
     {
-        int rem = decnum % 2;
-        decnum = decnum / 2;
+        int rem = decNum % 2;
+        decNum /= 2;
         ans += rem * pow;
         pow *= 10;
     }
-    cout << ans << endl;
+    return ans;
+}
+
+int main()
+{
+    int decnum = 43;
+    for (int i = 1; i <= 10; i++)
+    {
+        cout << dectobinary(i) << endl;
+    }
 
     return 0;
 }
